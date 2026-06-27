d894dc46b8484d555b71ef40dff6f55f00d55b6a
Ved-Dhanani <veddhanani@gmail.com>
2026-06-28 00:21:48 +0530

    Add files via upload

---
+++ test_currency.py
@@ -0,0 +1,7 @@
+from utils.currency import format_amount
+
+print(format_amount(1000, 'INR'))
+print(format_amount(1000, 'USD'))
+print(format_amount(1000, 'OMR'))
+print(format_amount(1000, 'EUR'))
+print(format_amount(1000, 'GBP'))
